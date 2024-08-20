5c04d8e363f6d1f876df5547ad154250f5f88c7f
HarryFett2k7 <95986296+HarryFett2k7@users.noreply.github.com>
2024-08-20 12:00:47 +0100

    Update main.py

--- BinaryConv/SimplePage/main.py
+++ BinaryConv/SimplePage/main.py
@@ -13,7 +13,7 @@ btoi = st.toggle("Binary to integer")
 if itob:
     number = st.text_input("Enter your number: ", value = 1)
     binary = bin(int(number))
-st.markdown("Binary number: " + str(binary[2:]))
+st.markdown("Binary number: ", (binary[2:]))
 
 if btoi:
     number = st.text_input("Enter binary value: ", value = 1)
